7b0df82fba54ba3a9ebfb6dfab8750297e782394
V. Zagurski <arbeitersolidaritat@gmail.com>
2026-03-25 16:12:26 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
+import time
 from MyVISA.SA_4051H import SA_4051H
 
-resource_name = 'TCPIP0::169.254.19.125::5025::SOCKET'
+resource_name = 'TCPIP0::164.16.116.124::5024::SOCKET'
 
 san = SA_4051H(resource_name)
 print(san._idn, san._status, sep='\n')
@@ -11,7 +12,7 @@ san.setup_meas(trac_type='MAXH',
                det='POS',
                filt='SHAP GAUS',
                fcent=950,
-               span=100,
+               span=500,
                rbw=100,
                vbw=100,
                points=1000,
@@ -22,5 +23,15 @@ san.setup_display(unit='DBM',
                   ref=0,
                   pdiv=10
                   )
+
 san.initiate()
+called = san._called
+
+while not called:
+    time.sleep(2)
+    freqs, data, called = san.get_data()
+    print(san._status)
+
+print(freqs, data, san._status, sep='\n')
+
 san.close()

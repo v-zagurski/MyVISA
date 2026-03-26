5fc91ce812a32e1671b0178b8c17bb9c7d177521
V. Zagurski <arbeitersolidaritat@gmail.com>
2026-03-26 08:59:15 +0300

    Update example.py

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from MyVISA.SA_4051H import SA_4051H
 resource_name = 'TCPIP0::164.16.116.124::5024::SOCKET'
 
 san = SA_4051H(resource_name)
-print(san._idn, san._status, sep='\n')
+print(san.idn, san.status, sep='\n')
 
 san.setup_meas(trac_type='MAXH',
                cont=0,
@@ -19,19 +19,19 @@ san.setup_meas(trac_type='MAXH',
                att=10,
                gain=0
                )
-san.setup_display(unit='DBM', 
+san.setup_display(unit='DBM',
                   ref=0,
                   pdiv=10
                   )
 
 san.initiate()
-called = san._called
+called = san.called
 
 while not called:
     time.sleep(2)
     freqs, data, called = san.get_data()
-    print(san._status)
+    print(san.status)
 
-print(freqs, data, san._status, sep='\n')
+print(freqs, data, san.status, sep='\n')
 
 san.close()
